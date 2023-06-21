57906b9cf3a767588cd05d726e8cd667adc94719
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-21 16:06:52 +0200

    login/register pages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import Add from '../img/addAvatar.png';
 
 const Register = () => {
     return (
         <div className='formContainer'>
-            <div className='formWrapper'></div>
-                <span className='logo'>Super Chat!</span>
+            <div className='formWrapper'>
+                <span className='logo'>Ceci est un Chat!</span>
                 <span className='title'>S'enregistrer</span>
             <form>
                 <input type='text' placeholder='pseudo'></input>
                 <input type='email' placeholder='email'></input>
                 <input type='password' placeholder='mot de passe'></input>
-                <input type='file'></input>
+                <input style={{display:"none"}} type='file' id='file/'></input>
+                <label htmlFor='file'>
+                    <img src={Add} alt=''></img>
+                    <span>Ajouter un avatar</span>
+                </label>
                 <button>S'enregistrer</button>
             </form>
             <p>Vous avez déjà un compte? Se connecter</p>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
