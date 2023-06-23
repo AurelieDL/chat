135e1a97c8da65108b09e58449f0a2eedb4372a8
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-23 11:07:01 +0200

    styles sidebar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Recherche"></input>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/573267/pexels-photo-573267.jpeg?auto=compress&cs=tinysrgb&w=400"
+          alt=""
+        ></img>
+        <div className="userChatInfo">
+          <span>Ken</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
