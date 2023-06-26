3c86933342027c11e5ccd4570f41d291ce516d6e
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-26 12:07:37 +0200

    correction

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>Ken</span>
-        <div className="chatIcons"></div>
+        <div className="chatIcons">
+          <img src={Cam} alt=""></img>
+          <img src={Add} alt=""></img>
+          <img src={More} alt=""></img>
+        </div>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import Add from '../img/addAvatar.png';
+import React from "react";
+import Add from "../img/addAvatar.png";
 
 const Register = () => {
-    return (
-        <div className='formContainer'>
-            <div className='formWrapper'>
-                <span className='logo'>Ceci est un Chat!</span>
-                <span className='title'>S'enregistrer</span>
-            <form>
-                <input type='text' placeholder='pseudo'></input>
-                <input type='email' placeholder='email'></input>
-                <input type='password' placeholder='mot de passe'></input>
-                <input style={{display:"none"}} type='file' id='file/'></input>
-                <label htmlFor='file'>
-                    <img src={Add} alt=''></img>
-                    <span>Ajouter un avatar</span>
-                </label>
-                <button>S'enregistrer</button>
-            </form>
-            <p>Vous avez déjà un compte? Se connecter</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Ceci est un Chat!</span>
+        <span className="title">S'enregistrer</span>
+        <form>
+          <input type="text" placeholder="pseudo"></input>
+          <input type="email" placeholder="email"></input>
+          <input type="password" placeholder="mot de passe"></input>
+          <input style={{ display: "none" }} type="file" id="file/"></input>
+          <label htmlFor="file">
+            <img src={Add} alt=""></img>
+            <span>Ajouter un avatar</span>
+          </label>
+          <button>S'enregistrer</button>
+        </form>
+        <p>Vous avez déjà un compte? Se connecter</p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
