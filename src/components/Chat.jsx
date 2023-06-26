3c9ad5eebaf2107e94f6f712af36ed3c809f01c7
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-26 11:50:46 +0200

    styles

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Ken</span>
+        <div className="chatIcons">
+          <img src={Cam} alt=""></img>
+          <img src={Add} alt=""></img>
+          <img src={More} alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
