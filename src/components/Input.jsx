50e0231601ed637df43e6991af7e3f2cf8393bdd
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-26 13:29:08 +0200

    desgin terminé

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Img from "../img/addPhoto.png";
+import Attach from "../img/attach.png";
 
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <div className="input">
+      <input type="text" placeholder="... Les blablas ici" />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Envoyer</button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
