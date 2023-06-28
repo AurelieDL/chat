8348a4f0086286c64dfab7a90ddc65c2fb504f7f
AurelieDL <91252299+AurelieDL@users.noreply.github.com>
2023-06-28 15:14:48 +0200

    routes/login/logout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Add from "../img/addAvatar.png";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Ceci est un Chat!</span>
         <span className="title">S'enregistrer</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="pseudo"></input>
           <input type="email" placeholder="email"></input>
           <input type="password" placeholder="mot de passe"></input>
